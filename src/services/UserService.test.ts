f29a86d4a83af84aaf2be747561e529e7b6fad9f
Rémi Weng <ixartz@users.noreply.github.com>
2022-05-05 18:59:03 +0200

    test: verify team feature is working in UserService

--- src/services/UserService.test.ts
+++ src/services/UserService.test.ts
@@ -7,7 +7,7 @@ import { UserService } from './UserService';
 describe('UserService', () => {
   describe('Get/Create User', () => {
     it("should return null when the user don't exist", async () => {
-      const userId = 'abc-123';
+      const userId = 'user-123';
 
       const userService = new UserService(getDbClient());
       const user = await userService.findByUserId(userId);
@@ -15,7 +15,7 @@ describe('UserService', () => {
     });
 
     it('should create a new user and be able to get the user from the db', async () => {
-      const userId = 'abc-123';
+      const userId = 'user-123';
 
       // Create a new user
       const userService = new UserService(getDbClient());
@@ -28,7 +28,7 @@ describe('UserService', () => {
     });
 
     it("should create a new user because the user don't exist", async () => {
-      const userId = 'abc-123';
+      const userId = 'user-123';
 
       const userService = new UserService(getDbClient());
       const user = await userService.findOrCreate(userId);
@@ -37,12 +37,11 @@ describe('UserService', () => {
     });
 
     it('should create a new user', async () => {
-      const userId = 'abc-123';
+      const userId = 'user-123';
 
       // Create a new user
       const userService = new UserService(getDbClient());
       let user = await userService.create(userId);
-      assert(user !== null, "user shouldn't be null");
 
       // Verify `findOrCreate` method doesn't create a new user
       userService.create = jest.fn();
@@ -53,12 +52,47 @@ describe('UserService', () => {
     });
 
     it("should throw an exception when the user don't exist", async () => {
-      const userId = 'abc-123';
+      const userId = 'user-123';
 
       const userService = new UserService(getDbClient());
       await expect(userService.strictFindByUserId(userId)).rejects.toThrow(
         /Incorrect UserID/
       );
+    });
+  });
+
+  describe('User and Team', () => {
+    it("shouldn't belong to the team by default", async () => {
+      const userId = 'user-123';
+      const teamId = 'team-123';
+
+      // Create a new user
+      const userService = new UserService(getDbClient());
+      await userService.create(userId);
+
+      // Verify team
+      await expect(
+        userService.findAndVerifyTeam(userId, teamId)
+      ).rejects.toThrow(/isn't a team member/);
+    });
+
+    it('should add user to the team', async () => {
+      const userId = 'user-123';
+      const teamId = 'team-123';
+
+      // Create a new user
+      const userService = new UserService(getDbClient());
+      let user = await userService.create(userId);
+
+      // Add user to the team
+      assert(user !== null, "user shouldn't be null");
+      user.addTeam(teamId);
+      await userService.update(user);
+
+      // Verify user belongs to the team
+      user = await userService.findAndVerifyTeam(userId, teamId);
+      assert(user !== null, "user shouldn't be null");
+      expect(user.id).toEqual(userId);
     });
   });
 });
