8f25f509645d31cfad948cc51068c3d4a44f77d3
Rémi Weng <ixartz@users.noreply.github.com>
2022-05-06 00:26:56 +0200

    test: add test for query in validation middleware

--- src/middlewares/Validation.test.ts
+++ src/middlewares/Validation.test.ts
@@ -1,10 +1,11 @@
+import { RequestHandler, Response } from 'express';
 import httpMocks from 'node-mocks-http';
 import { z } from 'zod';
 
 import { validateRequest } from './Validation';
 
 describe('Validation middleware', () => {
-  describe('Test validateRequest', () => {
+  describe('Empty object as schema', () => {
     it('should call `next` function', () => {
       const request = httpMocks.createRequest({
         method: 'GET',
@@ -16,52 +17,100 @@ describe('Validation middleware', () => {
       const validate = validateRequest({});
       validate(request, response, next);
 
-      expect(next).toHaveBeenCalled();
+      expect(next).toHaveBeenCalledTimes(1);
     });
   });
 
-  it('should throw an exception with an incorrect params', () => {
-    const request = httpMocks.createRequest({
-      method: 'GET',
-      url: '/',
-    });
-    const response = httpMocks.createResponse();
-    const next = jest.fn();
+  describe('Params request', () => {
+    let validate: RequestHandler;
+    let response: httpMocks.MockResponse<Response>;
+    let next: jest.Mock;
 
-    const validate = validateRequest<any>({
-      params: z.object({
-        teamId: z.string().nonempty(),
-      }),
+    beforeEach(() => {
+      validate = validateRequest<any>({
+        params: z.object({
+          teamId: z.string().nonempty(),
+        }),
+      });
+      response = httpMocks.createResponse();
+      next = jest.fn();
     });
 
-    expect(() => validate(request, response, next)).toThrow(
-      expect.objectContaining({
-        message: 'Error in request validation',
-        errorList: [{ param: 'teamId', type: 'invalid_type' }],
-      })
-    );
-    expect(next).not.toHaveBeenCalled();
+    it('should throw an exception with an incorrect params', () => {
+      const request = httpMocks.createRequest({
+        method: 'GET',
+        url: '/',
+      });
+
+      expect(() => validate(request, response, next)).toThrow(
+        expect.objectContaining({
+          message: 'Error in request validation',
+          errorList: [{ param: 'teamId', type: 'invalid_type' }],
+        })
+      );
+      expect(next).not.toHaveBeenCalled();
+    });
+
+    it('should call next and test `params` without errors', () => {
+      const request = httpMocks.createRequest({
+        method: 'GET',
+        url: '/',
+        params: {
+          teamId: 'team-123',
+        },
+      });
+
+      validate(request, response, next);
+
+      expect(next).toHaveBeenCalledTimes(1);
+    });
   });
 
-  it('should call next and test `params` without errors', () => {
-    const request = httpMocks.createRequest({
-      method: 'GET',
-      url: '/',
-      params: {
-        teamId: 'team-123',
-      },
-    });
-    const response = httpMocks.createResponse();
-    const next = jest.fn();
+  describe('Query request', () => {
+    let validate: RequestHandler;
+    let response: httpMocks.MockResponse<Response>;
+    let next: jest.Mock;
 
-    const validate = validateRequest<any>({
-      params: z.object({
-        teamId: z.string().nonempty(),
-      }),
+    beforeEach(() => {
+      validate = validateRequest<any>({
+        query: z.object({
+          email: z.string().nonempty().email(),
+        }),
+      });
+      response = httpMocks.createResponse();
+      next = jest.fn();
     });
 
-    validate(request, response, next);
+    it('should throw an exception with an incorrect query', () => {
+      const request = httpMocks.createRequest({
+        method: 'GET',
+        url: '/',
+        query: {
+          email: 'example.com',
+        },
+      });
 
-    expect(next).toHaveBeenCalled();
+      expect(() => validate(request, response, next)).toThrow(
+        expect.objectContaining({
+          message: 'Error in request validation',
+          errorList: [{ param: 'email', type: 'invalid_string' }],
+        })
+      );
+      expect(next).not.toHaveBeenCalled();
+    });
+
+    it('should call next and test `query` without errors', () => {
+      const request = httpMocks.createRequest({
+        method: 'GET',
+        url: '/',
+        query: {
+          email: 'email@example.com',
+        },
+      });
+
+      validate(request, response, next);
+
+      expect(next).toHaveBeenCalledTimes(1);
+    });
   });
 });
