d1259ff49fb6fdc23a9621cc13c129dcd546a74a
Rémi Weng <ixartz@users.noreply.github.com>
2022-09-12 21:37:50 +0200

    feat: add environment variable to control pino

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -2,5 +2,8 @@ import pino from 'pino';
 import { lambdaRequestTracker, pinoLambdaDestination } from 'pino-lambda';
 
 const destination = pinoLambdaDestination();
-export const logger = pino({ base: undefined }, destination);
+export const logger = pino(
+  { base: undefined, level: process.env.PINO_LOG_LEVEL || 'info' },
+  destination
+);
 export const withRequest = lambdaRequestTracker();
