c40e90d5d68f36083c9be0bafbdf35b27ca139e4
Rémi Weng <ixartz@users.noreply.github.com>
2022-09-13 03:09:05 +0200

    fix: make pino sync in local/offline environment

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -1,7 +1,17 @@
+import type { DestinationStream } from 'pino';
 import pino from 'pino';
 import { lambdaRequestTracker, pinoLambdaDestination } from 'pino-lambda';
 
-const destination = pinoLambdaDestination();
+import { Env } from './Env';
+
+let destination: DestinationStream = pinoLambdaDestination();
+
+if (Env.getValue('IS_OFFLINE', false)) {
+  destination = pino.destination({
+    sync: true,
+  });
+}
+
 export const logger = pino(
   { base: undefined, level: process.env.PINO_LOG_LEVEL || 'info' },
   destination
