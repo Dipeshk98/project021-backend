4929d2ee1b9cd5688760e1e16721b1c361a82817
Rupam-safespacelabs <rupam@safespacelabs.com>
2025-02-05 21:56:16 +0530

    backend port is change form 3000 to 4000 in docker
    Signed-off-by: Rupam-safespacelabs <rupam@safespacelabs.com>

--- prisma/migrations/20250123153412_y/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `contact_number` on the `members` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "members" DROP COLUMN "contact_number";
--- prisma/migrations/20250123172801_y/migration.sql
+++ prisma/migrations/20250123172801_y/migration.sql
@@ -1,10 +1,10 @@
 /*
   Warnings:
 
-  - You are about to drop the column `role1` on the `members` table. All the data in the column will be lost.
+  - You are about to drop the column `role2` on the `members` table. All the data in the column will be lost.
   - Added the required column `role` to the `members` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
-ALTER TABLE "members" DROP COLUMN "role1",
+ALTER TABLE "members" DROP COLUMN "role2",
 ADD COLUMN     "role" "Role" NOT NULL;
